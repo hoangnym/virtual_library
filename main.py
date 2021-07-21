038edc75882c539a7a5f0dbd02aac7b4bd43b0cf
hoangnym <hoangnym94@gmail.com>
2021-07-21 17:10:51 +0200

    add new book to list

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+#Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float(50), nullable=False)
+
+# Create initial database
+db.create_all()
 
 
 @app.route('/')
 def home():
+    # Read all records
+    all_books = db.session.query(Book).all()
     return render_template(
         "index.html",
         books=all_books
@@ -16,15 +32,14 @@ def home():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        data = request.form
-        book = {"name": data["book_name"],
-                "author": data["book_author"],
-                "rating": data["rating"]}
-        all_books.append(book)
-        print(all_books)
-
-        # NOTE: You can use the redirect method from flask to redirect to another route
-        # e.g. in this case to the home page after the form has been submitted.
+        # Add to database
+        book = Book(
+            title=request.form["book_name"],
+            author=request.form["book_author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(book)
+        db.session.commit()
         return redirect(url_for('home'))
 
     return render_template("add.html")
