c56fefd21926842c05a15455acb6d4be1c1c22a0
hoangnym <hoangnym94@gmail.com>
2021-07-21 15:23:01 +0200

    account for empty library

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ all_books = []
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        books=all_books
+    )
 
 
 @app.route("/add", methods=["GET", "POST"])
