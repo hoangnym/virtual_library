dc5098a194b703fb43c89f0f1a5aa1a223563bd4
hoangnym <hoangnym94@gmail.com>
2021-07-21 12:38:57 +0200

    add book class

---
+++ book.py
@@ -0,0 +1,5 @@
+class Book:
+    def __init__(self, name, author, rating):
+        self.name = name
+        self.author = author
+        self.rating = rating
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from book import Book
 
 app = Flask(__name__)
 
@@ -10,8 +11,11 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    print(request.method)
+    data = request.form
+    Book(data["book_name"], data["book_author"], data["rating"])
     return render_template("add.html")
 
 
