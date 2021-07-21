e8df20c452f34da6c453e1b24fb30e30f7054464
hoangnym <hoangnym94@gmail.com>
2021-07-21 22:14:36 +0200

    finish up editing functionality

--- main.py
+++ main.py
@@ -45,21 +45,22 @@ def add():
     return render_template("add.html")
 
 
-@app.route("/edit/<int:index>", methods=["GET","POST"])
-def edit(index):
+@app.route("/edit", methods=["GET","POST"])
+def edit():
+
     if request.method == "POST":
-        # Update database entry
-        # book = Book(
-        #     title=request.form["book_name"],
-        #     author=request.form["book_author"],
-        #     rating=request.form["rating"]
-        # )
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
         return redirect(url_for('home'))
 
-    book_to_update = Book.query.get(index)
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+
     return render_template(
         "edit.html",
-        book=book_to_update
+        book=book_selected
     )
 
 
